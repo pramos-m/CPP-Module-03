faafc7342caf572f1aab3b9b5436e7286e4be1f2
Pol Ramos <119877499+pramos-m@users.noreply.github.com>
2023-08-17 15:54:53 +0200

    00 done

--- ex00/inc/ClapTrap.hpp
+++ ex00/inc/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: pramos-m <pramos-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 15:25:16 by pramos-m          #+#    #+#             */
-/*   Updated: 2023/08/16 15:25:18 by pramos-m         ###   ########.fr       */
+/*   Updated: 2023/08/16 16:52:36 by pramos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,20 @@ class ClapTrap
 		ClapTrap( std::string name, unsigned int hPts, unsigned int ePts, \
 			unsigned int aDmg );
 		~ClapTrap( void );
+		ClapTrap&				operator=( const ClapTrap& clap );
+		void					attack( const std::string& target );
+		void					takeDamage( unsigned int amount );
+		void					beRepaired( unsigned int amount );
+		// Getters
+		std::string				getName( void ) const;
+		unsigned int			getHPts( void ) const;
+		unsigned int			getEPts( void ) const;
+		unsigned int			getADmg( void ) const;
+		// Setters
+		void					setName( std::string name );
+		void					setHPts( unsigned int hPts );
+		void					setEPts( unsigned int ePts );
+		void					setADmg( unsigned int aDmg );
 };
 
 std::ostream&	operator<<( std::ostream& out, ClapTrap& clap );
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,17 +6,79 @@
 /*   By: pramos-m <pramos-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 15:25:01 by pramos-m          #+#    #+#             */
-/*   Updated: 2023/08/16 15:25:44 by pramos-m         ###   ########.fr       */
+/*   Updated: 2023/08/17 15:52:43 by pramos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	<iostream>
 #include	<iomanip>
-#include	"ClapTrap.hpp"
+#include	"../inc/ClapTrap.hpp"
+
+#define FILL	40
+
+void	attacking( ClapTrap& clap1, ClapTrap& clap2 )
+{
+	bool	canAttack;
+
+	canAttack = clap1.getEPts() < 1 ? false : true;
+	clap1.attack( clap2.getName() );
+	if ( canAttack == true )
+		clap2.takeDamage( clap1.getADmg() );
+}
+
+void	war( ClapTrap& one, ClapTrap& two, int times )
+{
+	for ( int i = 0; i < times; i++ )
+	{
+		attacking( one, two );
+		attacking( two, one );
+	}
+}
+
+void	test( ClapTrap& one, ClapTrap& two, bool change_dmg )
+{
+	war( one, two, 1 );
+	if ( change_dmg == true )
+		one.setADmg( 1 );
+	attacking( one, two );
+	if ( change_dmg == true )
+		two.setADmg( 1 );
+	attacking( two, one );
+	if ( change_dmg == true )
+		one.setADmg( 8 );
+	attacking( one, two );
+	if ( change_dmg == true )
+		two.setADmg( 6 );
+	std::cout << std::endl;
+	one.beRepaired( one.getADmg() * 10 );
+	two.beRepaired( one.getADmg() * 10 );
+	std::cout << std::endl;
+	attacking( two, one );
+	war( one, two, 1 );
+	if ( change_dmg == true )
+	{
+		one.setADmg( 1 );
+		two.setADmg( 1 );
+	}
+	war( one, two, 6 );
+	std::cout << "\none -> " << one << std::endl;
+	std::cout << "two -> " << two << std::endl;
+}
+
+void	test_clap( void )
+{
+	ClapTrap	clap1( "clap1" );
+	ClapTrap	clap2( clap1 );
+
+	clap2.setName( "clap2" );
+	std::cout << "\none -> " << clap1 << std::endl;
+	std::cout << "two -> " << clap2 << "\n" << std::endl;
+	test( clap1, clap2, true );
+	std::cout << std::endl;
+}
 
 int	main( void )
 {
-	print_next_test( "ClapTrap test" );
 	test_clap();
 
 	return ( 0 );
